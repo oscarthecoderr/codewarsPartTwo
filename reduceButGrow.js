019f0fd297b57f3974018e6143d77404c5e4b819
oscarthecoderr <dev.oscarrios@gmail.com>
2023-01-04 20:17:21 -0500

    reduce but grow

---
+++ reduceButGrow.js
@@ -0,0 +1,6 @@
+//Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+function grow(x){
+  return x.reduce((total,num)=>{
+    return total * num
+  },1)
+  }
