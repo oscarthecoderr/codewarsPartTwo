589864fe434ee4f2f773b1aeecc8e43212efb3c4
oscarthecoderr <dev.oscarrios@gmail.com>
2023-01-25 20:28:31 -0500

    mangoes

---
+++ mangoes.js
@@ -0,0 +1,2 @@
+//There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+mango = (quantity,price) => price * ( quantity - Math.floor(quantity / 3))
